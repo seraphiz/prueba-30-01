f22cd603af221fa86bccc5aac318bfa955e20084
Cristian <cristian.arthur@hotmail.com>
2019-01-30 14:56:57 -0300

    final prueba

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   get '/todos/list', to: 'todos#list', as: 'lista/todos'
+  #resources :todos
   get '/todos', to: 'todos#index'
-  get '/todos/new', to: 'todos#new'
-  post '/todos', to: 'todos#create', as: 'create/todo'
+  get '/todos/new', to: 'todos#new', as: 'new/todos'
+  post '/todos', to: 'todos#create', as: 'create/todos'
   get '/todos/:id', to: 'todos#show', as: 'todo'
-  get '/todo/:id/edit', to: 'todos#edit', as: 'todo/edit'
+  get '/todo/:id/edit', to: 'todos#edit', as: 'edit_todo'
   patch '/todos/:id', to: 'todos#update'
-  delete '/todos/:id', to: 'todos#destroy', as: 'todo/delete'
+  delete '/todos/:id', to: 'todos#destroy', as: 'delete/todo'
   patch '/todos/:id/complete', to: 'todos#complete', as: 'complete/todos'
-
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'todos#index'
 end
