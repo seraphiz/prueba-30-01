4dee91282e3107f2171ca9b36b67c59992521b1a
Cristian <cristian.arthur@hotmail.com>
2019-01-30 11:25:26 -0300

    create

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,4 +7,19 @@ class TodosController < ApplicationController
     def new
         @todo = Todo.new
     end
+
+    def create
+        todo = Todo.new(todo_params)
+        todo.save
+        redirect_to root_path, notice: 'ToDo add correctly!'
+    end
+
+    private
+
+    def todo_params
+        
+        params.require(:todo).permit(:description)
+
+    end
+
 end
